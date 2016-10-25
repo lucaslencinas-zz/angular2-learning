fea5c14cf3b91f60c25e7bbb43377d94c6279a5f
Lucas Lencinas <lllencinas@gmail.com>
2016-10-25 13:58:11 -0300

    Step 7 Completed: HTTP

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,26 +1,39 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-
-import { AppComponent }   from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
-import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
+import { HttpModule }    from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { HeroSearchComponent }     from './hero-search.component';
+import { DashboardComponent }  from './dashboard.component';
+
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import './rxjs-extensions';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [HeroService],
   bootstrap: [
